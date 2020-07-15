5dff45edf5ce291544e6fb594b208e2f63524178
yzj <869737937@qq.com>
2020-07-15 18:00:17 +0800

    [add] honey-redis 中间件代码初始化

---
+++ src/main/java/com/eboy/honeyredis/HoneyRedisApplication.java
@@ -0,0 +1,16 @@
+package com.eboy.honeyredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author yangzhijie
+ */
+@SpringBootApplication
+public class HoneyRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HoneyRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eboy/honeyredis/HoneyRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eboy.honeyredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
